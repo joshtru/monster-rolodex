a00f36ebb4f25a0a2826d90b940bd92ae12beb5e
Joshua Okoro <joshuaojc@gmail.com>
2019-08-04 17:41:52 -0500

    Updates

---
+++ precache-manifest.a7ed1de0508cb0638a09f46a086875e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2d251ded19fad01d73de17110bdc1d",
+    "url": "/joshtru/monster-rolodex.git/index.html"
+  },
+  {
+    "revision": "efc35ef04d98450733ad",
+    "url": "/joshtru/monster-rolodex.git/static/css/main.52d06e7d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/joshtru/monster-rolodex.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "efc35ef04d98450733ad",
+    "url": "/joshtru/monster-rolodex.git/static/js/main.b885fa47.chunk.js"
+  },
+  {
+    "revision": "d080730eb1d551b31e56",
+    "url": "/joshtru/monster-rolodex.git/static/js/runtime~main.a953ea08.js"
+  }
+]);
